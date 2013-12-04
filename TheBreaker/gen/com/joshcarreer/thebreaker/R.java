ec81d21fd9888105cef3963196c756c16bb48c90
Josh Carreer <josh.carreer@gmail.com>
2013-12-04 09:06:59 -0500

    Created main activity layout & add pics

--- TheBreaker/gen/com/joshcarreer/thebreaker/R.java
+++ TheBreaker/gen/com/joshcarreer/thebreaker/R.java
@@ -24,14 +24,26 @@ public final class R {
         public static final int 2player=0x7f020000;
         public static final int 3player=0x7f020001;
         public static final int 4player=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
+        public static final int coin=0x7f020003;
+        public static final int d12=0x7f020004;
+        public static final int d20=0x7f020005;
+        public static final int d4=0x7f020006;
+        public static final int d6=0x7f020007;
+        public static final int dice=0x7f020008;
+        public static final int ic_launcher=0x7f020009;
+        public static final int rps=0x7f02000a;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
-        public static final int b_2player=0x7f080001;
-        public static final int b_3player=0x7f080002;
-        public static final int b_4player=0x7f080003;
-        public static final int player1wins=0x7f080000;
+        public static final int action_settings=0x7f080009;
+        public static final int b_2player=0x7f080006;
+        public static final int b_3player=0x7f080007;
+        public static final int b_4player=0x7f080008;
+        public static final int b_cointoss=0x7f080002;
+        public static final int b_dice=0x7f080000;
+        public static final int b_randnumber=0x7f080003;
+        public static final int b_rockpapersci=0x7f080001;
+        public static final int player1wins=0x7f080005;
+        public static final int tv_Main_1=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -47,7 +59,12 @@ public final class R {
         public static final int button_2player=0x7f050005;
         public static final int button_3player=0x7f050006;
         public static final int button_4player=0x7f050007;
+        public static final int button_cointoss=0x7f05000b;
+        public static final int button_dice=0x7f050009;
+        public static final int button_randnumber=0x7f05000a;
+        public static final int button_rockpapersci=0x7f050008;
         public static final int hello_world=0x7f050002;
+        public static final int main_header=0x7f05000c;
         public static final int playerselect_header=0x7f050004;
         public static final int title_activity_player_select=0x7f050003;
     }
